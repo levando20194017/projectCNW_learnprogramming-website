0c344525361e77f7116a82d0e31beed050f91f59
levando20194017 <levando194017@gmail.com>
2023-04-26 09:30:41 +0700

    api get users

--- Backend/src/controller/userController.js
+++ Backend/src/controller/userController.js
@@ -17,7 +17,24 @@ let handleLogin = async (req, res) => {
         user: userData.user ? userData.user : {}
     })
 }
+let handleGetAllUsers = async (req, res) => {
+    let id = req.body.id; // ALL, id
+    if (!id) {
+        return res.status(200).json({
+            errCode: 1,
+            errMessage: "Missing required parameters",
+            user: []
+        })
+    }
+    let users = await userService.getAllUsers(id);
+    return res.status(200).json({
+        errCode: 0,
+        errMessage: 'Ok',
+        users
+    })
+}
 
 module.exports = {
-    handleLogin: handleLogin
+    handleLogin: handleLogin,
+    handleGetAllUsers: handleGetAllUsers
 }
