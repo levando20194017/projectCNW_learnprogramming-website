6445c0c0cafcfa1c6ed0060e95f91289b61f7759
levando20194017 <levando194017@gmail.com>
2023-05-06 22:55:32 +0700

    delete and edit user

--- Frontend-React.JS/src/services/userService.js
+++ Frontend-React.JS/src/services/userService.js
@@ -10,4 +10,14 @@ const getAllUsers = (inputId) => {
 const createNewUserService = (data) => {
     return axios.post('/api/create-new-user', data)
 }
-export { handleLoginApi, getAllUsers, createNewUserService }
+
+const deleteUserService = (userId) => {
+    return axios.delete('/api/delete-user', {
+        data: { id: userId }
+    })
+}
+
+const editUserService = (data) => {
+    return axios.put('/api/edit-user', data)
+}
+export { handleLoginApi, getAllUsers, createNewUserService, deleteUserService, editUserService }
