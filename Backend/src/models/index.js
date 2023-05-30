cf92e4f9b3af3f05ae1c3bdf6e8a06fda4441098
levando20194017 <levando194017@gmail.com>
2023-05-31 00:44:15 +0700

    update config.js .env to connect DB online

--- Backend/src/models/index.js
+++ Backend/src/models/index.js
@@ -1,20 +1,46 @@
 'use strict';
-
-require('dotenv').config(); //import package 
+require('dotenv').config();
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+// const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+const customizeConfig = {
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: 'postgres',
+  logging: false,
+  dialectOptions:
+    process.env.DB_SSL === 'true' ?
+      {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false
+        }
+      } : {}
+  ,
+  query: {
+    "raw": true
+  },
+  timezone: "+07:00"
 }
+
+sequelize = new Sequelize(
+  process.env.DB_DATABASE_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  customizeConfig);
+
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+
+//   sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
 
 fs
   .readdirSync(__dirname)
