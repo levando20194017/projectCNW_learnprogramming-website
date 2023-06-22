57218574c20dce2f0c25b6a495c6fc3928a28e80
disane1311 <diep.vtbwork@gmail.com>
2023-06-23 01:28:56 +0700

    header-update-beta

--- Frontend-React.JS/src/containers/Header/D_header_user.js
+++ Frontend-React.JS/src/containers/Header/D_header_user.js
@@ -5,6 +5,8 @@ import * as actions from "../../store/actions";
 import { adminMenu } from './menuApp';
 import { Link } from 'react-router-dom';
 
+import { AppBar, Toolbar, Typography } from '@mui/material';
+
 import './D_header_user.scss';
 
 class userHeader extends Component {
@@ -17,43 +19,49 @@ class userHeader extends Component {
                     <div id="headerUser-left-logo">
                         <img src={`${process.env.PUBLIC_URL}/assets/img/logo-symbol.png`} alt='avatar' style={{height: "40px", borderRadius: "50%"}} />                        
                         <div id='headerUser-left-logo-p'>
-                            CODECRUSH
+                            CodeCrush
                         </div>
                     </div>
                 </div>
 
                 <div id="headerUser-center">
-                <ul id="headerUser-nav">
+                    <ul id="headerUser-nav">
                         <li ><a href="">HOME</a></li>
                         <li ><a href="">BLOG</a></li>
                         <li><a href="">ABOUT US</a></li>
                     </ul>
                 </div>
 
-                <div id='headerUser-right'>
-                    <div id='headerUser-right-container'>
-                        <img id='headerUser-right-avt' src={userInfo?.img_url} alt='avatar' style={{height: "40px", borderRadius: "50%"}} />
-                        <Link to='/'>
-                        <div className="btn btn-logout" onClick={processLogout}>
-                            <i className="fas fa-sign-out-alt"></i>
-                        </div>
-                        </Link>
-                    </div>
+                <div id='headerUser-right'>                   
+                    <ul id='headerUser-right-nav'>
+                            <li>
+                                <a href="#">
+                                    Khóa học của tôi
+                                </a>
+                                <ul id="subnav-courses">
+                                    <li><a href="">HTML CSS cơ bản</a></li>
+                                    <li><a href="">ReactJS cơ bản</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img className="headerUser-right-avt" src={userInfo?.img_url} alt='avatar' width={40} height={40} />
+                                </a>
+                                <ul id="subnav-avatar">
+                                    <li><a href="">Trang cá nhân</a></li>
+                                    <li><a href="">Đổi mật khẩu</a></li>
+                                    <li><a href="" onClick={processLogout}><Link to='/'>Đăng xuất</Link></a></li>
+                                </ul>
+                            </li> 
+                            {/* <Link to='/'>
+                                <div className="btn btn-logout" onClick={processLogout}>
+                                    <i className="fas fa-sign-out-alt"></i>
+                                </div>
+                            </Link> */}
+                    </ul>
                 </div>
             </div>
-            // <div className="header-container">
-            //     {/* thanh navigator */}
-            //     <div className="header-tabs-container">
-            //         <Navigator menus={adminMenu} />
-            //     </div>
-            //     {/* <div>Xin chao</div> */}
-
-            //     {/* nút logout */}
-            //     {userInfo && userInfo.fullName ? <div>Xin chào {userInfo.fullName}</div> : ""}
-            //     <div className="btn btn-logout" onClick={processLogout}>
-            //         <i className="fas fa-sign-out-alt"></i>
-            //     </div>
-            // </div>
+            
         );
     }
 
